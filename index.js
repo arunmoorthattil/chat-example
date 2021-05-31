2be5b2a709137f5d9978d51ac01c15bb31c25601
arunmoorthattil <43172183+arunmoorthattil@users.noreply.github.com>
2021-05-31 23:29:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var express = require('express')
 var cors = require('cors')
 var app = express()
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http,{
+  cors: {
+    origin: "https://learnmyskills.com",
+    methods: ["GET", "POST"]
+  }
+});
+
+
 const port = process.env.PORT || 3000;
 
 app.use(cors())
