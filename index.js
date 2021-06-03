35ebb56bdefd982804493674196bedd40e982e90
arunmoorthattil <43172183+arunmoorthattil@users.noreply.github.com>
2021-06-03 20:31:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,139 +27,38 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-      clients[data.username] = {
-      "socket": socket.id
-    };
-  
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-});
-
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
-
-// Entire gameCollection Object holds all games and info
-
-var gameCollection =  new function() {
-
-  this.totalGameCount = 0,
-  this.gameList = []
-
-};
-
-function buildGame(socket) {
-
-
- var gameObject = {};
- gameObject.id = (Math.random()+1).toString(36).slice(2, 18);
- gameObject.playerOne = socket.username;
- gameObject.playerTwo = null;
- gameCollection.totalGameCount ++;
- gameCollection.gameList.push({gameObject});
-
- console.log("Game Created by "+ socket.username + " w/ " + gameObject.id);
- io.emit('gameCreated', {
-  username: socket.username,
-  gameId: gameObject.id
-});
-
-
-}
-
-function killGame(socket) {
-
-  var notInGame = true;
-  for(var i = 0; i < gameCollection.totalGameCount; i++){
-
-    var gameId = gameCollection.gameList[i]['gameObject']['id']
-    var plyr1Tmp = gameCollection.gameList[i]['gameObject']['playerOne'];
-    var plyr2Tmp = gameCollection.gameList[i]['gameObject']['playerTwo'];
-    
-    if (plyr1Tmp == socket.username){
-      --gameCollection.totalGameCount; 
-      console.log("Destroy Game "+ gameId + "!");
-      gameCollection.gameList.splice(i, 1);
-      console.log(gameCollection.gameList);
-      socket.emit('leftGame', { gameId: gameId });
-      io.emit('gameDestroyed', {gameId: gameId, gameOwner: socket.username });
-      notInGame = false;
-    } 
-    else if (plyr2Tmp == socket.username) {
-      gameCollection.gameList[i]['gameObject']['playerTwo'] = null;
-      console.log(socket.username + " has left " + gameId);
-      socket.emit('leftGame', { gameId: gameId });
-      console.log(gameCollection.gameList[i]['gameObject']);
-      notInGame = false;
-
-    } 
-
-  }
-
-  if (notInGame == true){
-    socket.emit('notInGame');
-  }
-
-
-}
-
-function gameSeeker (socket) {
-  ++loopLimit;
-  if (( gameCollection.totalGameCount == 0) || (loopLimit >= 20)) {
-
-    buildGame(socket);
-    loopLimit = 0;
-
-  } else {
-    var rndPick = Math.floor(Math.random() * gameCollection.totalGameCount);
-    if (gameCollection.gameList[rndPick]['gameObject']['playerTwo'] == null)
-    {
-      gameCollection.gameList[rndPick]['gameObject']['playerTwo'] = socket.username;
-      socket.emit('joinSuccess', {
-        gameId: gameCollection.gameList[rndPick]['gameObject']['id'] });
-
-      console.log( socket.username + " has been added to: " + gameCollection.gameList[rndPick]['gameObject']['id']);
-
-    } else {
-
-      gameSeeker(socket);
-    }
-  }
-}
-
 
 // Chatroom
 
 var numUsers = 0;
 
-io.on('connection', function (socket) {
-  var addedUser = false;
 
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
+io.on('connection', (socket) => {
+ 
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
   });
 
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function (username) {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
+      clients.push(username);
     ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
+     // echo globally (all clients) that a person has connected
+    io.emit('user joined', {
+      userList: clients,
       numUsers: numUsers
     });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
+  });
+
+  // when the client emits '  leave user', this listens and executes
+  socket.on('leave user', function (username) {
+    --numUsers;
+    removeItemFromArray(clients,username);
+    io.emit('user left', {
+      userList: clients,
       numUsers: numUsers
     });
   });
@@ -169,63 +68,24 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function () {
     if (addedUser) {
       --numUsers;
-      killGame(socket);
-
+    removeItemFromArray(clients,username);
       // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
+     io.emit('user left', {
+        userList: clients,
         numUsers: numUsers
       });
     }
   });
 
+function removeItemFromArray(array, n) {
+    const index = array.indexOf(n);
 
-  socket.on('joinGame', function (){
-    console.log(socket.username + " wants to join a game");
+    // if the element is in the array, remove it
+    if(index > -1) {
 
-    var alreadyInGame = false;
-
-    for(var i = 0; i < gameCollection.totalGameCount; i++){
-      var plyr1Tmp = gameCollection.gameList[i]['gameObject']['playerOne'];
-      var plyr2Tmp = gameCollection.gameList[i]['gameObject']['playerTwo'];
-      if (plyr1Tmp == socket.username || plyr2Tmp == socket.username){
-        alreadyInGame = true;
-        console.log(socket.username + " already has a Game!");
-
-        socket.emit('alreadyJoined', {
-          gameId: gameCollection.gameList[i]['gameObject']['id']
-        });
-
-      }
-
+        // remove item
+        array.splice(index, 1);
     }
-    if (alreadyInGame == false){
-
-
-      gameSeeker(socket);
-      
-    }
-
-  });
-
-
-  socket.on('leaveGame', function() {
-
-
-    if (gameCollection.totalGameCount == 0){
-     socket.emit('notInGame');
-     
-   }
-
-   else {
-    killGame(socket);
-  }
-
+    return array;
+}
 });
-
-});
-
-
-
-
-
